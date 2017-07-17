dbc4e7f077f5aff272a8cdabf2c2b5b066574671
freejoe76 <jmurphy@denverpost.com>
2017-07-17 17:23:37 -0400

    ☂️☂️☂️☂️☂️☂️

--- flask/application/__init__.py
+++ flask/application/__init__.py
@@ -35,7 +35,7 @@ with app.app_context():
     app.page = page
     app.sitename = ''
     app.chapters_all = ['01','02','03','04','05','06','07','08','09','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','epilogue','acknowledgements', 'prologue']
-    app.chapters = ['01','02','03','04','05','acknowledgements']
+    app.chapters = ['01','02','03','04','05','06','07','08','09','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','epilogue','acknowledgements']
     app.metadata = metadata_all
 
 import application.flatpage
